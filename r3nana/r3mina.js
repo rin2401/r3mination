aa90cb20748ae9f81eec6f59f20cd7bc602dba99
rin2401 <rin2401@gmail.com>
2020-10-15 13:47:01 +0700

    Open anime from avatar anime in anilist

--- r3nana/r3mina.js
+++ r3nana/r3mina.js
@@ -1,19 +1,47 @@
 console.log("From r3mina with love <3")
 
 // host = "http://localhost:5000"
-host = "http://r3mina.herokuapp.com"
-
+host = "http://r3mina.herokuapp.com"    
 
 function replace(){
+
+    iframe = document.getElementsByTagName('iframe')[0]
+    console.log(iframe)
+
+    content = iframe.contentWindow.document.body.innerHTML
+
+    console.log(content)
+
     paths = window.location.pathname.split("/")
+    id = paths[paths.length-3]
+    name = paths[paths.length-2]
+    if(!id) return
+    console.log(id, name)
+
+    cover = document.querySelectorAll("img.cover")[0]
+    console.log(cover)
+
+    if (cover) {
+
+        cover.onmouseenter = function(){
+            this.style.filter = "blur(1px)";
+        }
+
+        cover.onmouseout = function(){
+            this.style.filter = "";
+        } 
+
+        cover.addEventListener("click", function(){
+            path = `${host}/anime/${name}`
+            console.log(path)
+            window.open(path)
+        });
+    }
+
     if (paths[paths.length-1] != "watch") {
         console.log("Not watch page")
         return
     }
-    id = paths[paths.length-3]
-    name = paths[paths.length-2]
-    console.log(id, name)
-
 
     eps = document.getElementsByClassName("episode")
     for(ep of eps) {
@@ -31,11 +59,18 @@ function replace(){
     }
 
 }
-
-function replace_timeout(time=1000) {
+function replace_timeout(time=10) {
     setTimeout(replace, time)
 }
+
+replace_timeout(500)
+
 
 replace_timeout(1000)
 
 window.onclick = replace_timeout
+
+// links = document.querySelectorAll("link")
+// links.addEventListener("click", replace_timeout)
+
+
