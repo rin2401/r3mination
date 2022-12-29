f7718dbfde0b75a83ccaefffe0257d7d74f487f5
rin2401 <rin2401@gmail.com>
2022-12-30 02:16:15 +0700

    Update remove fb ads
    - Change selector post
    - Change detect sponsor by size of use svg

--- r3ads/r3ads.js
+++ r3ads/r3ads.js
@@ -4,20 +4,57 @@ BLACK = ["khuyến mãi"]
 ACTIVITY = ["commented", "comment", "shared", "replied", "interested", "marked", "tagged", "premiere"]
 EXCEPT = ["linh", "quốc", "nguyen"]
 
+ADS = ["grabfood", "baemin", "shoppefood", "hotline", "ads/about", "send message", "địa chỉ"]
+
 function remove_newline(text) {
     return text.replace(/(\r\n|\n|\r)/gm, " ")
 }
 
+function hower() {
+    elements = document.querySelectorAll("div[aria-posinset]  use")
+    for(element of elements) {
+        console.log(element + element.getBBox().width + "  " + element.getBBox().height)
+        var event = new MouseEvent('mouseover', {
+          'view': window,
+          'bubbles': true,
+          'cancelable': true
+        });
+
+        // DISPATCHING THE EVENT, i.e., ACTUALLY HOVERING
+        element.dispatchEvent(event);
+    }
+}
+
+function is_sponsored_box(article) {
+    boxes = article.querySelectorAll("use")
+    for (box of boxes) {
+        w = box.getBBox().width
+        if (w > 56 && w < 57) return true
+    }
+    return false
+}
+
 function remove() {
     chrome.storage.sync.get(["count", "sponsored", "suggest", "activity", "share", "black", "tab"], function(settings) {
-        articles = document.querySelectorAll('[role="article"]')
+        // hower()
+        // articles = document.querySelectorAll('[role="article"]')
+        articles = document.querySelectorAll('div[aria-posinset]')
+        // console.log("articles: " + articles.length)
+
+
+
         for(article of articles) {
             text = article.innerText.split("·")
             text_inline = text[0].replace(/(\r\n|\n|\r)/gm, "")
+            // console.log(text)
 
             // const regex = new RegExp('S.*p.*o.*n.*s.*o.*r.*e.*d');
             // is_sponsored = regex.test(text_inline) || text_inline.includes("Sponsored")
-            is_sponsored = Array.from("Sponsored").every(x => text_inline.includes(x))
+            // is_sponsored = Array.from("Sponsored").every(x => text_inline.includes(x))
+
+            // is_sponsored = article.innerHTML.constain("/ads/about")
+            text_lower =  article.innerHTML.toLowerCase()
+            is_sponsored = ADS.some(v => text_lower.includes(v)) || is_sponsored_box(article)
             if(is_sponsored && settings.sponsored) {
                 console.log("%cSponsored " + article.getAttribute("aria-posinset"), "font-weight: bold; color: red;", remove_newline(article.innerText))
                 article.remove()
